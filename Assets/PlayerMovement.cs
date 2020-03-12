3ca9619d5c6a1467d6f6fc3944afe6fa2e2b5f7f
Matthew Long <MattLong87@gmail.com>
2020-03-12 18:22:12 -0400

    added MatthewL Test Scene with basic player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float sidewaysForce = 10f;
+    void FixedUpdate () {
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector2(sidewaysForce * Time.deltaTime, 0), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector2(-sidewaysForce * Time.deltaTime, 0), ForceMode2D.Force);
+        }
+        
+    }
+}
